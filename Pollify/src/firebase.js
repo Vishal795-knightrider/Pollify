bc1bc3ddb7345c2523bd79c1d95fa9fb0e6f0558
Vishal795-knightrider <vk3293801@gmail.com>
2026-03-04 22:52:01 +0530

    Move Firebase config to environment variables

--- Pollify/src/firebase.js
+++ Pollify/src/firebase.js
@@ -3,14 +3,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBWZ3N-_ym_5SX-UbXv7lvEeFUrGLjkVJk",
-  authDomain: "pollify-e197f.firebaseapp.com",
-  projectId: "pollify-e197f",
-  storageBucket: "pollify-e197f.firebasestorage.app",
-  messagingSenderId: "453420493394",
-  appId: "1:453420493394:web:3cf30dfa9efa1f56d5cd8b"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
